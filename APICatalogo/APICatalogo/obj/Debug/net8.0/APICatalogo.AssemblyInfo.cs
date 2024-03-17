2bf5a99c5177a912d4dbce45ab6fb2bb94602aa9
Maxuel Vieira Tobá Junior <maxueltstz@hotmail.com>
2024-03-17 12:19:42 -0300

    Finalizando o CRUD de Categoria e Produtos, e começando aprender sobre os middlewares

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0f355e8869557a3a175240ee1f1c07310bb8da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5aef1a459fed9f7705210e2014a64573fe855ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
