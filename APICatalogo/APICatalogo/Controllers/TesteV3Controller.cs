cb62f937901153a6ee50305b21bbc41474fba5db
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2024-03-25 14:05:55 -0300

    Aprendendo sobre versionamento de api utilizando o Asp.ApiExplorer, versionamento em query strings e em URL

---
+++ APICatalogo/APICatalogo/Controllers/TesteV3Controller.cs
@@ -0,0 +1,26 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("api/teste")]
+[ApiController]
+[ApiVersion("3.0")]
+[ApiVersion("4.0")]
+public class TesteV3Controller : ControllerBase
+{
+    [MapToApiVersion(3)]
+    [HttpGet]
+    public string GetVersion3()
+    {
+        return "TesteV3 - GET - Api Versão 3.0";
+    }
+
+    [MapToApiVersion(4)]
+    [HttpGet]
+    public string GetVersion4()
+    {
+        return "TesteV4 - GET - Api Versão 4.0";
+    }
+}
